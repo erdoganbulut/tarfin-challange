b31d776c7fb5004f035157acc91a32cadb39f628
Erdoğan Bulut <erdoganbulut@Erdogan-MacBook-Pro.local>
2019-03-14 06:32:47 +0300

    Fix home(list) page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,63 @@
 <template lang="pug">
   .home
-    table.table
+    table.table(v-if="loaded")
       thead.thead-dark
         tr
           th(scope="col") #
-          th(scope="col") First
-          th(scope="col") Last
-          th(scope="col") Handle
-      tbody
-        tr
-          th(scope="row") 1
-          td Mark
-          td Otto
-          td @mdo
-        tr
-          th(scope="row") 2
-          td Jacob
-          td Thornton
-          td @fat
-        tr
-          th(scope="row") 3
-          td Larry
-          td the Bird
-          td @twitter
+          th(scope="col") thumb
+          th(scope="col") Name
+          th(scope="col") Date
+          th(scope="col") status
+          th(scope="col")
+      tbody(v-if="!loading")
+        tr(v-for="(item, i) in list")
+          th(scope="row") {{ page.number * 5 + i + 1 }}
+          td
+            img(:src="getThumbnail(item.images).url")
+          td {{ item.name }}
+          td(v-if="item.dates.start.localDate")
+            span {{ item.dates.start.localDate }}
+            span(v-if="item.dates.start.localDate")  {{ item.dates.start.localTime }}
+            span(v-if="item.dates.end")
+              span(v-if="item.dates.end.localDate")  - {{ item.dates.end.localDate }}
+              span(v-if="item.dates.end.localTime")  {{ item.dates.end.localTime }}
+            span(v-if="item.dates.timezone")
+              br
+              |  ({{ item.dates.timezone }})
+          td {{ item.dates.status.code }}
+          td Detay
+
+    Pagination(v-if="loaded")
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
+import _ from 'lodash';
+import Pagination from '../components/Pagination.vue';
+
 export default {
   name: 'home',
+  computed: {
+    ...mapGetters({
+      list: 'list/list',
+      page: 'list/page',
+      loading: 'list/loading',
+      loaded: 'list/loaded',
+    }),
+  },
+  methods: {
+    ...mapActions({
+      getList: 'list/getList',
+    }),
+    getThumbnail(images) {
+      return _.minBy(images, 'width');
+    },
+  },
+  mounted() {
+    this.getList({ keyword: 'P!NK' });
+  },
+  components: {
+    Pagination,
+  },
 };
 </script>
